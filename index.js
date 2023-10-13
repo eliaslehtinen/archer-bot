14084d348c6ff23522953a90beb789360770324f
Elias <elias.a.lehtinen@gmail.com>
2023-10-13 13:30:09 +0300

    Create client and log in

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Main file for the Discord bot
+
+// Import dependencies
+const Discord = require('discord.js');
+const { prefix } = require('./config.json');
+const ytdl = require('ytdl-core');
+const token = process.env.ARCHERBOT_TOKEN;
+
+// Create client and log in
+const client = new Discord.Client();
+client.login(token);
+
+// Add listeners
+Client.once('ready', () => {
+    console.log('Ready!');
+})
+Client.once('reconnecting', () => {
+    console.log('Reconnecting!');
+})
+Client.once('disconnect', () => {
+    console.log('Disconnect!');
+})
