ee2d3a14fccf50637b65f993b647e540f30ebae9
Elias <elias.a.lehtinen@gmail.com>
2023-10-13 13:50:30 +0300

    Add message handling

--- index.js
+++ index.js
@@ -25,3 +25,25 @@ client.once('reconnecting', () => {
 client.once('disconnect', () => {
     console.log('Disconnect!');
 })
+
+// Listen for new messages
+client.on('message', async message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const serverQueue = queue.get(message.guild.id);
+
+    if (message.content.startsWith('${prefix}play')) {
+        execute(message, serverQueue);
+        return;
+    } else if (message.content.startsWith('${prefix}skip')) {
+        skip(message, serverQueue);
+        return;
+    } else if (message.content.startsWith('${prefix}stop')) {
+        stop(message, serverQueue);
+        return;
+    } else {
+        message.channel.send('You need to enter a valid command!\n'+
+        'Available commands are !play, !skip and !stop.')
+    }
+})
