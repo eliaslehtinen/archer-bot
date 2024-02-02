b76dd26de6233710fd2adb9b0bc960eb4a9d18fe
Elias <elias.a.lehtinen@gmail.com>
2024-02-02 19:37:20 +0200

    Working bot with /ping, /server and /user

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 // Main file for the Discord bot
 
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');    // Node's native file system. Used to read the commands directory
+const path = require('node:path') // Node's native path utility module
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const internal = require('node:stream');
+
+// Access token from environment variable
+// Alternative to using a config.json file
 const token = process.env.ARCHERBOT_TOKEN;
-const prefix = require("./config.json")     // Prefix used in front of bot commands
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+// Load command files
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+// Listener for receiving slash command interactions
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+        }
+    }
+});
 
 // When the client is ready, run this code once
 client.once(Events.ClientReady, readyClient => {
@@ -15,3 +65,5 @@ client.once(Events.ClientReady, readyClient => {
 
 // Log in to Discord with token
 client.login(token);
+
+// TODO: Registering slash commands
